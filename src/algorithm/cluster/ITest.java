bba6895bda26d944801dfff2e2db59380db5ca30
zhangbo <zhangbo@admaster.com.cn>
2014-08-13 18:26:47 +0800

    test add file

---
+++ src/algorithm/cluster/ITest.java
@@ -0,0 +1,7 @@
+package algorithm.cluster;
+
+/**
+ * Created by boyce on 2014/8/13.
+ */
+public class ITest {
+}
---
+++ src/algorithm/distance/ITest.java
@@ -0,0 +1,7 @@
+package algorithm.distance;
+
+/**
+ * Created by boyce on 2014/8/13.
+ */
+public class ITest {
+}
